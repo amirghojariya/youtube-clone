98202a336eb98ea446ca929f7ea6ec615459a13d
amirghojariya <ghojariyaamir@gmail.com>
2025-11-26 18:36:15 +0530

    basic layout

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -1,15 +1,14 @@
+import { HomeLayout } from "../modules/home/ui/layouts/home-layout";
+
 interface LayoutProps {
     children: React.ReactNode
 }
 
 const Layout = ({children}: LayoutProps) => {
-    return (
-        <div>
-            <div className="p-4 bg-rose-500 w-full">
-                I am Navbar
-            </div>
+    return ( 
+        <HomeLayout>
             {children}
-        </div>
+        </HomeLayout>
      );
 }
 
